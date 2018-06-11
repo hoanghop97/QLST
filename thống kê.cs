0251ec3a3b76f3737d16b668ac87728ce7ea3f98
nguyentiend199 <38046125+nguyentiend199@users.noreply.github.com>
2018-06-11 15:24:44 +0700

    cap nhat thong ke

---
+++ thống kê.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Quản_lý_bán_hàng
+{
+    public partial class formTK : Form
+    {
+        public formTK()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-NL36GS2\SQLEXPRESS;Initial Catalog=Quanlybanhang;Integrated Security=True");
+
+        private void btnthoat_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmMain ftk = new frmMain();
+            ftk.ShowDialog();
+            this.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void formTK_Load(object sender, EventArgs e)
+        {
+            Hienthi();
+        }
+        private void Hienthi()
+        {
+
+            string sql = " SELECT * from vwHangTonCH";
+            SqlDataAdapter ad = new SqlDataAdapter(sql, conn);
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+    }
+}
+
+   
+   
